d28290c67c8d1f7561ca02f44a285baa8feadd05
David <retrochanel97@gmail.com>
2020-10-27 21:45:38 -0600

    Fetching to API for get the list of crypto currencys

--- components/Form.js
+++ components/Form.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+
 import {Picker} from '@react-native-picker/picker';
+import Axios from 'axios';
 
 const Form = () => {
+  const [Currency, setCurrency] = useState('');
+  const [Crypto, setCrypto] = useState('');
+  const [Cryptos, setCryptos] = useState([]);
+
+  const getCurrency = (currency) => {
+    setCurrency(currency);
+  };
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      let url =
+        'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+      let res = await Axios.get(url);
+      setCryptos(res.data.Data);
+    };
+    fetchAPI();
+  }, []);
+
   return (
     <>
       <View>
         <Text style={styles.label}>Moneda</Text>
-        <Picker>
+        <Picker
+          onValueChange={(currency) => getCurrency(currency)}
+          selectedValue={Currency}>
           <Picker.Item label="- Selecciona -" value="" />
           <Picker.Item label="USD" value="USD" />
           <Picker.Item label="MXN" value="MXN" />
